745cc2b0716590d2bf99669e4972d226c9f49cd0
thinh2507 <117496244+thinh250702@users.noreply.github.com>
2024-03-26 11:28:13 +0700

    Add project files.

---
+++ AnanasMVCWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace AnanasMVCWebApp.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
